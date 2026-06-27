5eed747545142792be0edf43913c87910af5e65c
Max <hamidzonovmuhriddin@gmail.com>
2026-06-27 17:39:44 +0900

    feat: create routing system

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  type ThunkAction,
+  type Action,
+} from "@reduxjs/toolkit";
+// import HomePageReducer from "./screens/homePage/slice";
+// import ProductsPageReducer from "./screens/productsPage/slice";
+// import OrdersPageReducer from "./screens/ordersPage/slice";
+import reduxLogger from "redux-logger";
 
-const store = configureStore({
-  reducer: {},
+export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    // @ts-ignore
+    getDefaultMiddleware().concat(reduxLogger),
+  reducer: {
+    //   homePage: HomePageReducer,
+    //   productsPage: ProductsPageReducer,
+    //   ordersPage: OrdersPageReducer,
+  },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
