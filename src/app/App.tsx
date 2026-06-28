8e0167d5365905e2040b659de9126a6cd9b4b21e
Max <hamidzonovmuhriddin@gmail.com>
2026-06-28 16:48:33 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,18 @@
 import "../css/app.css";
 import { Route, Switch, useLocation } from "react-router-dom";
-import { HomePage } from "./screens/homePage";
+// import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage/index";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
 import { Footer } from "./components/footer";
+import "../css/app.css";
+import "../css/navbar.css";
+import { HelpPage } from "./screens/helpPage";
 
 function App() {
   const location = useLocation();
-  console.log("location", location);
 
   return (
     <>
@@ -25,8 +27,11 @@ function App() {
         <Route path="/member-page">
           <UserPage />
         </Route>
+        <Route path="/help">
+          <UserPage />
+        </Route>
         <Route path="/">
-          <HomePage />
+          <HelpPage />
         </Route>
       </Switch>
       <Footer />
