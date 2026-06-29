5c21f989ccbd3139de4e5c8cd5b2b730ffebf8e5
Max <hamidzonovmuhriddin@gmail.com>
2026-06-29 15:13:35 +0900

    feat: integration footer and basket source

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,3 +1,88 @@
-export function Footer() {
-  return <div>Footer</div>;
+import styled from "@emotion/styled";
+import { Box, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const Footers = styled.div`
+  width: 100%;
+  height: 590px;
+  display: flex;
+  background: #343434;
+  background-size: cover;
+`;
+
+export default function Footer() {
+  const authMember = null;
+
+  return (
+    <Footers>
+      <Container>
+        <Stack flexDirection={"row"} sx={{ mt: "94px" }}>
+          <Stack flexDirection={"column"} style={{ width: "340px" }}>
+            <Box>
+              <img width={"100px"} src={"/icons/burak.svg"} />
+            </Box>
+            <Box className={"foot-desc-txt"}>
+              Focusing on the gourmet Turkish breakfast as well as the youth
+              society, CZN Burak Gurme aims to bring Turkish cuisine back. CZN
+              Burak Gurme creates an illusion with its cuisine.
+            </Box>
+            <Box className="sns-context">
+              <img src={"/icons/facebook.svg"} />
+              <img src={"/icons/twitter.svg"} />
+              <img src={"/icons/instagram.svg"} />
+              <img src={"/icons/youtube.svg"} />
+            </Box>
+          </Stack>
+          <Stack sx={{ ml: "288px" }} flexDirection={"row"}>
+            <Stack>
+              <Box>
+                <Box className={"foot-category-title"}>Bo'limlar</Box>
+                <Box className={"foot-category-link"}>
+                  <NavLink to="/">Home</NavLink>
+                  <NavLink to="/products">Products</NavLink>
+                  {authMember && <NavLink to="/orders">Orders</NavLink>}
+                  <NavLink to="/help">Help</NavLink>
+                </Box>
+              </Box>
+            </Stack>
+            <Stack sx={{ ml: "100px" }}>
+              <Box>
+                <Box className={"foot-category-title"}>Find us</Box>
+                <Box
+                  flexDirection={"column"}
+                  sx={{ mt: "20px" }}
+                  className={"foot-category-link"}
+                  justifyContent={"space-between"}
+                >
+                  <Box flexDirection={"row"} className={"find-us"}>
+                    <span>L.</span>
+                    <div>Downtown, Dubai</div>
+                  </Box>
+                  <Box className={"find-us"}>
+                    <span>P.</span>
+                    <div>+971 4 554 7777</div>
+                  </Box>
+                  <Box className={"find-us"}>
+                    <span>E.</span>
+                    <div>devexuz@gmail.com</div>
+                  </Box>
+                  <Box className={"find-us"}>
+                    <span>H.</span>
+                    <div>Visit 24 hours</div>
+                  </Box>
+                </Box>
+              </Box>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Stack
+          style={{ border: "1px solid #C5C8C9", width: "100%", opacity: "0.2" }}
+          sx={{ mt: "80px" }}
+        ></Stack>
+        <Stack className={"copyright-txt"}>
+          © Copyright Devex Global, All rights reserved.
+        </Stack>
+      </Container>
+    </Footers>
+  );
 }
